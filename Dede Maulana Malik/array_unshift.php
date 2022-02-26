e53bae8c709766a795bd15f9c592b989fe7b7bdf
Dede Maulana Malik <92030618+DedeMaulanaMalik@users.noreply.github.com>
2022-02-26 04:19:49 -0800

    Tugas Pemweb2 PHP

---
+++ Dede Maulana Malik/array_unshift.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+
+    $tims = ["erwin", "heru", "ali", "zaki"];
+    array_unshift($tims);
+    foreach($tims as $person) {
+        echo $person.'<br/>';
+    }
+?>
+</body>
+</html>
